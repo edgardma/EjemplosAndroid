424460efe5d80b38e72fb4c9cc781348065c55dd
Edgard M. Marquez Aylas <edgardma@gmail.com>
2020-11-06 23:53:59 -0500

    3er Ejemplo

---
+++ 1.Curso/RegistroDeSuperheroes/app/src/main/java/pe/com/dyd/example/registrodesuperheroes/MainActivity.java
@@ -0,0 +1,26 @@
+package pe.com.dyd.example.registrodesuperheroes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button registerButton = (Button) findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detailIntent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(detailIntent);
+            }
+        });
+    }
+}
