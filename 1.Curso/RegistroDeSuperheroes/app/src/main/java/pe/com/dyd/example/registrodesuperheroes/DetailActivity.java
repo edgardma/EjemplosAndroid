d7b8608c1df926dc28d861cf6b8285a0e633e198
Edgard M. Marquez Aylas <edgardma@gmail.com>
2020-11-08 08:08:40 -0500

    Terminar el Ejemplo de Registro de Heros

--- 1.Curso/RegistroDeSuperheroes/app/src/main/java/pe/com/dyd/example/registrodesuperheroes/DetailActivity.java
+++ 1.Curso/RegistroDeSuperheroes/app/src/main/java/pe/com/dyd/example/registrodesuperheroes/DetailActivity.java
@@ -3,6 +3,7 @@ package pe.com.dyd.example.registrodesuperheroes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Bundle extras = getIntent().getExtras();
+
+        String name = extras.getString(MainActivity.NAME);
+        String lastName = extras.getString(MainActivity.LAST_NAME);
+        String heroName = extras.getString(MainActivity.HERO_NAME);
+        String age = extras.getString(MainActivity.AGE);
+        String address = extras.getString(MainActivity.ADDRESS);
+        String city = extras.getString(MainActivity.CITY);
+
+        TextView heroData = (TextView)findViewById(R.id.hero_data);
+        heroData.setText(name + ", " +
+                lastName + ", " +
+                heroName + ", " +
+                age + ", " +
+                address + ", " +
+                city);
     }
 }
--- 1.Curso/RegistroDeSuperheroes/app/src/main/java/pe/com/dyd/example/registrodesuperheroes/MainActivity.java
+++ 1.Curso/RegistroDeSuperheroes/app/src/main/java/pe/com/dyd/example/registrodesuperheroes/MainActivity.java
@@ -6,8 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NAME = "name";
+    public static final String LAST_NAME = "last_name";
+    public static final String HERO_NAME = "hero_name";
+    public static final String AGE = "age";
+    public static final String ADDRESS = "address";
+    public static final String CITY = "city";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +23,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button registerButton = (Button) findViewById(R.id.register_button);
+
+        EditText nameEdit = (EditText)findViewById(R.id.name_edit);
+        EditText lastNameEdit = (EditText)findViewById(R.id.last_name_edit);
+        EditText heroNameEdit = (EditText)findViewById(R.id.hero_name_edit);
+        EditText ageEdit = (EditText)findViewById(R.id.age_edit);
+        EditText addressEdit = (EditText)findViewById(R.id.address_edit);
+        EditText cityEdit = (EditText)findViewById(R.id.city_edit);
+
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent detailIntent = new Intent(MainActivity.this, DetailActivity.class);
+
+                detailIntent.putExtra(NAME, nameEdit.getText().toString());
+                detailIntent.putExtra(LAST_NAME, lastNameEdit.getText().toString());
+                detailIntent.putExtra(HERO_NAME, heroNameEdit.getText().toString());
+                detailIntent.putExtra(AGE, ageEdit.getText().toString());
+                detailIntent.putExtra(ADDRESS, addressEdit.getText().toString());
+                detailIntent.putExtra(CITY, cityEdit.getText().toString());
+
                 startActivity(detailIntent);
             }
         });
