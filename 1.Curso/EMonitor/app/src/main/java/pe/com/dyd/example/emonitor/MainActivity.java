99b6af6fa006ab94059054e833b0e84e187efbd7
Edgard M. Marquez Aylas <edgardma@gmail.com>
2020-12-13 21:48:48 -0500

    56 - HttpRequest I

--- 1.Curso/EMonitor/app/src/main/java/pe/com/dyd/example/emonitor/MainActivity.java
+++ 1.Curso/EMonitor/app/src/main/java/pe/com/dyd/example/emonitor/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 import pe.com.dyd.example.emonitor.adapter.EarthquakeAdapter;
@@ -29,5 +34,39 @@ public class MainActivity extends AppCompatActivity {
 
         EarthquakeAdapter earthquakeAdapter = new EarthquakeAdapter(this, R.layout.eq_list_item, earthquakeList);
         earthquake_list_view.setAdapter(earthquakeAdapter);
+    }
+
+    private String downloadData(URL url) throws IOException {
+        String jsonResponse = "";
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000); // Miliseconds
+            urlConnection.setConnectTimeout(15000); // Miliseconds
+            urlConnection.connect();
+
+            inputStream = urlConnection.getInputStream();
+            jsonResponse = readFromStream(inputStream);
+        } catch (IOException ex) {
+            Toast.makeText(this, "Hubo un error al descargar los datos de terremotos " + ex.toString(),
+                    Toast.LENGTH_SHORT).show();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+
+        return jsonResponse;
+    }
+
+    private String readFromStream(InputStream inputStream) {
+        return "";
     }
 }
