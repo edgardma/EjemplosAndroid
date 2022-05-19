7abfa82c5629e5256b01f0f105e5016b2afbbd37
Edgard M. Marquez Aylas <edgardma@gmail.com>
2022-05-19 11:10:52 -0500

    Actualizacion del app: Mejoras en el código - Implementación Data Binding

--- 1.Curso/MiEdadCanina/app/src/main/java/pe/com/dyd/example/miedadcanina/MainActivity.java
+++ 1.Curso/MiEdadCanina/app/src/main/java/pe/com/dyd/example/miedadcanina/MainActivity.java
@@ -5,48 +5,35 @@ import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import pe.com.dyd.example.miedadcanina.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int CANT_ANIOS_PERRO = 6;
+    public static final int CANT_ANIOS_PERRO = 7;
     public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        final TextView answerText = (TextView) findViewById(R.id.answer_text);
-
-        final ImageView dogImage = (ImageView) findViewById(R.id.dog_image);
-        dogImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dog));
-
-        final EditText edadEdit = (EditText) findViewById(R.id.edad_edit);
-
-        final Button pulsameButton = (Button) findViewById(R.id.pulsame_button);
-
+        binding.dogImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dog));
         Log.d(TAG, "La app fue creada");
 
-        pulsameButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String edadString = edadEdit.getText().toString();
-                try {
-                    int edadInt = Integer.parseInt(edadString);
-                    edadInt = edadInt * CANT_ANIOS_PERRO;
-                    String respuesta = getString(R.string.answer);
-                    answerText.setText(String.format(respuesta, String.valueOf(edadInt)));
-                    Log.i(TAG, "Se calculó la edad");
-                } catch (NumberFormatException e) {
-                    Log.w(TAG, "Se ingresó un texto que no es número.");
-                    Toast.makeText(MainActivity.this, getString(R.string.error_message), Toast.LENGTH_LONG).show();
-                }
+        binding.pulsameButton.setOnClickListener(view -> {
+            String edadString = binding.edadEdit.getText().toString();
+            try {
+                int edadInt = Integer.parseInt(edadString);
+                edadInt = edadInt * CANT_ANIOS_PERRO;
+                String respuesta = getString(R.string.answer);
+                binding.answerText.setText(String.format(respuesta, String.valueOf(edadInt)));
+                Log.i(TAG, "Se calculó la edad");
+            } catch (NumberFormatException e) {
+                Log.w(TAG, "Se ingresó un texto que no es número.");
+                Toast.makeText(MainActivity.this, getString(R.string.error_message), Toast.LENGTH_LONG).show();
             }
         });
     }
